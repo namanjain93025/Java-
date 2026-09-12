f38c9cda6347c79efbfc68e15b97e70d9e22bd44
Naman Jain <namanjain93025@gmail.com>
2026-09-12 18:40:46 +0530

    Add Java OOPs project

---
+++ src/com/Oops/tut1/Basics.java
@@ -0,0 +1,11 @@
+package com.Oops.tut1;
+
+public class Basics {
+    public  static void main(String []args){
+        long a = Integer.MAX_VALUE;
+        long b = 1;
+        char c = 'a';
+        a = (int)c;
+        System.out.print(c+" "+a);
+    }
+}
