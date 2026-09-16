b0b752e82d5d0d999c1a1280258e06db284f1d95
Naman Jain <namanjain93025@gmail.com>
2026-09-16 16:45:22 +0530

    Exception handling 8

---
+++ src/com/Exception/class1/LaunchEH7.java
@@ -0,0 +1,11 @@
+package com.Exception.class1;
+class A{
+
+}
+public class LaunchEH7  {
+    public static void main(String[] args) throws InterruptedException{
+        System.out.println("main started");
+        Thread.sleep(5000);
+        System.out.println("main ended");
+    }
+}
---
+++ src/com/Exception/class1/LaunchEH8.java
@@ -0,0 +1,22 @@
+package com.Exception.class1;
+class X{
+    public void x() {
+        System.out.println("this is x");
+    }
+}
+class Y extends X{
+    public void x() throws ArithmeticException {
+        System.out.println("this is class  y");
+    }
+}
+//class Z extends X{
+//    public void x() throws InterruptedException {  not allowed
+//        System.out.println("this is class  Z");
+//    }
+//}
+public class LaunchEH8 {
+
+    public static void main(String[] args){
+
+    }
+}
