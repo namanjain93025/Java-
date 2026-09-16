08cd8f7bf70818eb4d286537fbdd0fa2f7a85052
Naman Jain <namanjain93025@gmail.com>
2026-09-16 16:55:23 +0530

    Exception handling 9

---
+++ src/com/Exception/class1/LaunchEH9.java
@@ -0,0 +1,32 @@
+package com.Exception.class1;
+
+import java.io.IOException;
+import java.sql.SQLException;
+
+class X1{
+    public void x()  throws  InterruptedException//Exception//InterruptedException    only parent of check exception or
+    {
+        System.out.println("this is x");
+    }
+}
+class Y1 extends X1{
+    public void x() throws ArithmeticException {
+        System.out.println("this is class  y");
+    }
+}
+class Z1 extends X1{
+    public void x() throws InterruptedException {
+        System.out.println("this is class  Z");
+    }
+}
+class V1 extends X1{
+    public void x(){
+        System.out.println("this is function v");
+    }
+}
+public class LaunchEH9 {
+
+    public static void main(String[] args){
+
+    }
+}
